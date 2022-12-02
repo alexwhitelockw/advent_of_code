19a055e7aea5d2742c731fe67a49f5b1e705d22f
alexwhitelockw <alexwhitelockw@gmail.com>
2022-12-02 13:30:52 +1100

    Part two for day one

--- advent_of_code_2022/day_one/scripts/aoc_day_one.py
+++ advent_of_code_2022/day_one/scripts/aoc_day_one.py
@@ -13,4 +13,19 @@ aoc_data_one_summary = aoc_data_one_data.groupby(["elf"]).aggregate(
     sum_calories=pd.NamedAgg("Calories", "sum")
 )
 
-aoc_data_one_summary.max()
+max_calorie_count = aoc_data_one_summary.max()  # Part One Answer
+
+print(f"The largest calorie count for an Elf is {int(max_calorie_count.values)}")
+
+aoc_data_one_summary["calorie_rank"] = aoc_data_one_summary.rank(
+    method="average",
+    ascending=False,
+)
+
+top_3_calorie_total = (  # Part Two Answer
+    aoc_data_one_summary
+        .loc[aoc_data_one_summary["calorie_rank"].isin([1,2,3])]
+        .aggregate(total_calories=pd.NamedAgg("sum_calories", "sum"))
+)
+
+print(f"The total calories across the top 3 Elves (based on calorie counts) is {int(top_3_calorie_total.values)}")
